b6111fcc4de4ee0c219e747600d26072371f621d
Hoang-Ngo <mhoang107@gmail.com>
2021-04-15 01:28:40 +0700

    mod

--- HelloWorld.cpp
+++ HelloWorld.cpp
@@ -7,6 +7,6 @@ int main()
   cout << "Hello World!" << endl;
   cout << "This file was modified" << endl;
   cout << "This file was modified again" << endl;
-  cout << "This file was modified again 2" << endl;
+  cout << "This file was modified again 1" << endl;
   return 0;
 }
