84701fabb23ea2f93b1de93bd0f5448a7d3c35d4
HoangNM <mhoang107@gmail.com>
2023-11-29 06:19:57 +0700

    fix

--- HelloWorld.cpp
+++ HelloWorld.cpp
@@ -6,7 +6,5 @@ int main()
 {
   cout << "Hello World!" << endl;
   cout << "This file was modified" << endl;
-  cout << "This file was modified again" << endl;
-  cout << "This file was modified again 1" << endl;
   return 0;
 }
